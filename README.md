090566dc1645b58b0dc8fbcfb1a3bbd36a10c372
Carlos Afonso Perez <cafonsop@amazon.com>
2020-07-27 22:20:40 +0200

    Add Custom Labels project ARN param to helper scripts

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 ```bash
 export GOV_ID_VAL_SAM_S3_BUCKET=<your S3 bucket for storing SAM artifacts>
 export GOV_ID_VAL_CFN_STACK_NAME=<your CloudFormation stack name>
+export GOV_ID_VAL_CUSTOM_LABELS_PROJECT_ARN=<the ARN of your Rekognition Custom Labels project>
 
 ./deploy-backend.sh
 
--- deploy-backend.sh
+++ deploy-backend.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-echo "GOV_ID_VAL_SAM_S3_BUCKET:  $GOV_ID_VAL_SAM_S3_BUCKET"
-echo "GOV_ID_VAL_CFN_STACK_NAME: $GOV_ID_VAL_CFN_STACK_NAME"
+echo "GOV_ID_VAL_SAM_S3_BUCKET:             $GOV_ID_VAL_SAM_S3_BUCKET"
+echo "GOV_ID_VAL_CFN_STACK_NAME:            $GOV_ID_VAL_CFN_STACK_NAME"
+echo "GOV_ID_VAL_CUSTOM_LABELS_PROJECT_ARN: $GOV_ID_VAL_CUSTOM_LABELS_PROJECT_ARN"
 
 sam package \
 	--s3-bucket $GOV_ID_VAL_SAM_S3_BUCKET \
@@ -10,4 +11,5 @@ sam package \
 sam deploy \
 	--template-file packaged.yaml \
 	--stack-name $GOV_ID_VAL_CFN_STACK_NAME \
-	--capabilities CAPABILITY_IAM
+	--capabilities CAPABILITY_IAM \
+	--parameter-overrides "CustomLabelsProjectVersionArn=$GOV_ID_VAL_CUSTOM_LABELS_PROJECT_ARN"
